3ec744ad6fa77e5496afb530af791534efc8cd61
fbcbarbosa <fbcbarbosa@gmail.com>
2014-04-19 20:22:38 -0300

    MP LABX Project created

---
+++ Interface-OV2640.X/build/XC16_24FJ256GA110/production/main.o.d
@@ -0,0 +1,4 @@
+build/XC16_24FJ256GA110/production/main.o:  \
+main.c  \
+system.h  \
+user.h 
---
+++ Interface-OV2640.X/main.c
@@ -0,0 +1,49 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__XC16__)
+    #include <xc.h>
+#elif defined(__C30__)
+    #if defined(__PIC24E__)
+    	#include <p24Exxxx.h>
+    #elif defined (__PIC24F__)||defined (__PIC24FK__)
+	#include <p24Fxxxx.h>
+    #elif defined(__PIC24H__)
+	#include <p24Hxxxx.h>
+    #endif
+#endif
+
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp              */
+
+/******************************************************************************/
+/* Global Variable Declaration                                                */
+/******************************************************************************/
+
+/* i.e. uint16_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+
+int16_t main(void)
+{
+
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize IO ports and peripherals */
+    InitApp();
+
+    /* TODO <INSERT USER APPLICATION CODE HERE> */
+
+    while(1)
+    {
+
+    }
+}
